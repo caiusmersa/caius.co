b122734815b4a997c0f3c75059f0e0a98fc3563b
Caius Mersa <caiusmersa@gmail.com>
2015-03-06 11:23:19 -0800

    initial version. sticks and stones

---
+++ README.md
@@ -0,0 +1,17 @@
+# Deployment
+
+## Dependencies
+### Grunt
+Install grunt.
+http://gruntjs.com/
+
+```
+npm install -g grunt-cli
+```
+
+### Grunt FTP Deploy
+
+```
+npm install grunt-ftp-deploy --save-dev
+```
+
---
+++ gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt) {
+
+// Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: "../DragonFractal/", src: ["*.html", "*.js"], dest: 'deploy/pages/'},
+          {expand: true, cwd: "source/", src: "**", dest: 'deploy/'},
+        ],
+      },
+    },
+    'ftp-deploy': {
+      build: {
+          auth: {
+              host: 'counterbeing.com',
+              port: 21,
+              authKey: 'caius.co'
+          },
+          src: 'deploy',
+          dest: 'html',
+          forceVerbose: true,
+          exclusions: ['**/.DS_Store']
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-ftp-deploy');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Default task(s).
+  grunt.registerTask('deploy', ['copy', 'ftp-deploy']);
+};
